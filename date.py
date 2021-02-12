d7edb0c757c7ee1e81667e057cd5978c4c3662ae
jonghyouk yun <ageldama@gmail.com>
2021-02-12 19:25:45 +0900

    --wip-- [skip ci]

--- date.py
+++ date.py
@@ -5,8 +5,10 @@ from PIL import Image, ImageDraw, ImageFont
 
 from datetime import datetime
 import time
-
 from random import randint
+import itertools
+import math
+
 
 
 def fmt_rgb(r, g, b):
@@ -26,15 +28,35 @@ def rand_rgb():
             fmt_rgb(r_i, g_i, b_i),)
 
 
-import itertools
-import math
-
 def radians():
     return (math.radians(i) for i in itertools.cycle(range(0, 360)))
 
-r = radians()
-while True:
-    print(next(r))
+
+class Mover:
+    def __init__(self,
+                 cur_pos=0,
+                 max_pos=120,
+                 direction=1,
+                 accel_gen=radians):
+        self.cur_pos = cur_pos
+        self.max_pos = max_pos
+        self.direction = direction
+        self.accel_gen = accel_gen()
+
+    def __next__(self):
+        new_pos = self.cur_pos + (self.direction * next(self.accel_gen))
+        
+        if new_pos <= 0:
+            new_pos = 0
+            self.direction *= -1
+        elif new_pos >= self.max_pos:
+            new_pos = self.max_pos
+            self.direction *= -1
+
+        self.cur_pos = new_pos
+
+        return new_pos
+    
 
 def main():
     LCD = LCD_1in44.LCD()
@@ -53,20 +75,23 @@ def main():
     image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
     draw = ImageDraw.Draw(image)
 
+    mover = Mover(max_pos=LCD.height - 1 - (fnt_size * 2))
+
     while True:
         rgb, rgb_i = rand_rgb()
+        rgb, rgb_i = '#000000', '#ffbf00'
 
         draw.rectangle((0, 0, LCD.width-1, LCD.height-1), fill=rgb)
 
         s = datetime.now().strftime('%H:%M:%S\n%f')
-        draw.multiline_text((0, LCD.height - 1 - (fnt_size * 2)), s,
-                            align='center',
-                            fill=rgb_i, font=fnt)
+        draw.multiline_text((0, next(mover)),
+                            s, align='center', fill=rgb_i, font=fnt)
 
         LCD.LCD_ShowImage(image,0,0)
         LCD_Config.Driver_Delay_ms(1)
 
-        time.sleep(0.1)
+        time.sleep(0.3)
+
 
 if __name__ == '__main__':
     main()
